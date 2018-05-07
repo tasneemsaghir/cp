35cb95eff388582853b63f197b8e77b79c13e5ad
tasneemsaghir <tasneemsaghir@github.com>
2018-05-07 23:19:24 +0530

    Initial Commit

---
+++ src/com/cp/arithematic/Swap.java
@@ -0,0 +1,22 @@
+package com.cp.arithematic;
+public class Swap {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=10,b=20;
+		a=a+b;
+		b=a-b;
+		a=a-b;
+		System.out.println("value of a is: "+a+" value of b is: "+b);
+
+	}
+
+}
+
+
+//how to create package -  package with package name(like com.sample (inside com folder sample folder created))
+//For Class name always use camel case notation.
+//Class name should be always noun and function name should be verb.
+
+
